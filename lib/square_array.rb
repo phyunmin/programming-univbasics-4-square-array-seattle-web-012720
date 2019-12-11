ee062d293d91528389afaf481cfea7038ded526b
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-11 23:39:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   buffer = []
   while counter < array.length do
-    array[counter] = (array[counter])**2
+    buffer.push((array[counter])**2)
     counter+=1
   end
 end
